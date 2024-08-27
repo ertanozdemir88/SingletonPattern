991c4a334ddb890b7ac58a3fbfbd15a801b0c793
ertanozdemir88 <ertanozdemir88@gmail.com>
2024-08-27 12:10:43 +0300

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Logger
+    {
+        private static Logger _instance;
+        private static readonly object _lock = new object();
+        private readonly string _logFilePath;
+
+        private Logger()
+        {
+            _logFilePath = "log.txt";
+            File.WriteAllText(_logFilePath, "Log file initialized.\n");
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Logger();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            string logMessage = $"{DateTime.Now}: {message}\n";
+            File.AppendAllText(_logFilePath, logMessage);
+            Console.WriteLine("Log message: " + logMessage);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using SingletonPattern;
+
+Logger logger1 = Logger.Instance;
+logger1.Log("First log message.");
+
+// Retrieve the same instance of Logger
+Logger logger2 = Logger.Instance;
+logger2.Log("Second log message.");
+
+// Demonstrate that both instances are the same
+bool sameInstance = ReferenceEquals(logger1, logger2);
+Console.WriteLine($"Are both instances the same? {sameInstance}");
+
+Console.ReadLine();
